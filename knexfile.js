78cc8f97729d7cce672b965d9dcc7df7d330f21d
divbyseptember <47344117+divbySeptember@users.noreply.github.com>
2019-05-19 17:48:48 -0400

    testing...

--- knexfile.js
+++ knexfile.js
@@ -14,10 +14,8 @@ module.exports = {
     seeds: { directory: './database/seeds' },
   },
   testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/test.sqlite3',
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
