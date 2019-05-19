8b5b31dc449d7d5da945ca0c4c7f9fab6839032d
divbyseptember <47344117+divbySeptember@users.noreply.github.com>
2019-05-19 18:01:05 -0400

    ...

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
-const dbEnv = process.env.DB_ENV || 'testing';
+const dbEnv = process.env.DB_ENV || 'production';
 
 module.exports = knex(knexConfig[dbEnv]);
